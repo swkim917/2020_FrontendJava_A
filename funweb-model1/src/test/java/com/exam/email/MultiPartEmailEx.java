15127596aba3450334d31ba1141f9c47f01ac48e
admin <admin@DESKTOP-DCCG7PF>
2021-02-01 12:34:54 +0900

    3. third upload

--- funweb-model1/src/test/java/com/exam/email/MultiPartEmailEx.java
+++ funweb-model1/src/test/java/com/exam/email/MultiPartEmailEx.java
@@ -10,55 +10,43 @@ public class MultiPartEmailEx {
 
 		long beginTime = System.currentTimeMillis();
 
-		// MultiPartEmail ��ü �غ�
 		MultiPartEmail email = new MultiPartEmail();
 		
-		// SMTP ���� ���ἳ��
 		email.setHostName("smtp.naver.com");
 		email.setSmtpPort(465);
 		email.setAuthentication("zencoding", "******");
 		
-		// ���ȿ��� SSL, TLS ��� ����
 		email.setSSLOnConnect(true);
 		email.setStartTLSEnabled(true);
 		
 		String response = "fail";
 		
 		
-		// ÷�������� ���� EmailAttachment ��ü�� ÷������ ������ŭ �غ�
 		EmailAttachment attach = new EmailAttachment();
 		attach.setPath("D:/����/images/hobbang.jpg");
-		attach.setDescription("ȣ���� �̹���"); // ÷������ ����
-//		attach.setName("ȣ����.jpg"); // ���ϸ��� �ٸ��̸����� �����ϱ�
+		attach.setDescription("ȣ���� �̹���");
+//		attach.setName("ȣ����.jpg");
 		
 		EmailAttachment attach2 = new EmailAttachment();
 		attach2.setPath("D:/����/images/mario.jpg");
 		
 		try {
-			// ������ ��� ���� (SMTP ���� �α��� ���� ���̵�� �����ϰ� �ؾ��Կ� ����!)
 			email.setFrom("zencoding@naver.com", "������", "utf-8");
 			
-			// �޴� ��� ����
 			email.addTo("swkim917@daum.net", "����", "utf-8");
 			email.addTo("swkim917@naver.com");
 			
-			// �޴� ���(������) ����
 			email.addCc("swkim917@gmail.com");
 			
-			// �޴� ���(���� ������) ����
 			email.addBcc("swkim917@nate.com");
 			
-			// ���� ����
 			email.setSubject("÷������ ���� �����Դϴ�.");
 			
-			// ���� ����
 			email.setMsg("÷������ ���� �����Դϴ�.\n�ι�° ���Դϴ�.\n\n����° ���Դϴ�.");
 			
-			// ÷������ �߰��ϱ�
 			email.attach(attach);
 			email.attach(attach2);
 			
-			// ���� �����ϱ�
 			response = email.send();
 			
 		} catch (EmailException e) {
--- funweb-model1/src/test/java/com/exam/email/SimpleEmailEx.java
+++ funweb-model1/src/test/java/com/exam/email/SimpleEmailEx.java
@@ -7,37 +7,20 @@ public class SimpleEmailEx {
 
 	public static void main(String[] args) {
 		
-		// CommonsEmail ���̺귯���� ����ؼ� �̸��� ���۱�� ����
-		//  -> JavaMail ���̺귯���� ����
-		//  -> JAF(Java Activation Framework) ���̺귯���� ����
-		
-		// SimpleEmail Ŭ���� : �ؽ�Ʈ �޽����� �����ϴ� �뵵
-		// MultiPartEmail Ŭ���� : �ؽ�Ʈ �޽����� ������ �Բ� ����
-		// HtmlEmail Ŭ���� : �̸����� HTML �������� �����ϴ� �뵵
-		
-		// �̸��� ���񽺴� ���� ���ۿ� ���������� SMTP�� �����.
-		// SMTP ���� ȣ��Ʈ��(IP�ּ�)
-		// SMTP ���� ��Ʈ: �⺻��Ʈ 465(SSL) �Ǵ� 587(TLS)
-		
-		
 		long beginTime = System.currentTimeMillis();
 		
-		// SimpleEmail ��ü �غ�
 		SimpleEmail email = new SimpleEmail();
 		
-		// SMTP ���� ���ἳ��
 		email.setHostName("smtp.naver.com");
 		email.setSmtpPort(465);
-		email.setAuthentication("zencoding", "i2green;");
+		email.setAuthentication("zencoding", "******");
 		
-		// ���ȿ��� SSL, TLS ��� ����
 		email.setSSLOnConnect(true);
 		email.setStartTLSEnabled(true);
 		
 		String response = "fail";
 		
 		try {
-			// ������ ��� ���� (SMTP ���� �α��� ���� ���̵�� �����ϰ� �ؾ��Կ� ����!)
 			email.setFrom("zencoding@naver.com", "관리자", "utf-8");
 			
 			email.addTo("swkim917@daum.net", "김상우", "utf-8");
